f15ba8c39a136539b9e7e00d1e325420263425d5
mayanksh99 <mayankshakya992@gmail.com>
2021-10-07 23:56:33 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/img/background.jpg')",
+      },
+      boxShadow: {
+        "2xl": "0 0 1rem #ffc600",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
